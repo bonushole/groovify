81a3620c2bdcdd23a9d416e4f012e8d336632c06
Maximilian Evans <156max@gmail.com>
2019-01-05 00:10:14 -0500

    alright we're gonna be really careful now

--- src/main/java/com/coolgroup/jukebox/MyController.java
+++ src/main/java/com/coolgroup/jukebox/MyController.java
@@ -7,6 +7,7 @@ import org.springframework.boot.ApplicationRunner;
 //import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
@@ -19,8 +20,23 @@ public class MyController{// implements ApplicationRunner{
 	@RequestMapping(value = "/")
 	public String hello() {
 	   //return gameUpdater.getGame().getItems();
-		return "suck my pp";
+		
+		Authenticator authenticator  = new Authenticator();
+		
+		return authenticator.authorizationCodeUri_Async();
+		
+		//return "suck my pp";
 	
+	}
+	@RequestMapping(value = "/callback")//, consumes = "text/plain" )
+	public String callBack() {//@RequestBody String payload) {
+		System.out.println("Something happened");
+		return "something happened";
+		//return payload;
+		
+		//Authenticator authenticator = new Authenticator();
+		
+		//return "";
 	}
 	/**
 	@Override
