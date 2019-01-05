8b465c8727a6e8c1a546406e9d3f6ca04dc40af2
Maximilian Evans <156max@gmail.com>
2019-01-04 23:55:20 -0500

    working through Oauth

--- src/main/java/com/coolgroup/jukebox/MyController.java
+++ src/main/java/com/coolgroup/jukebox/MyController.java
@@ -7,13 +7,10 @@ import org.springframework.boot.ApplicationRunner;
 //import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
-
-import com.wrapper.spotify.SpotifyApi;
-import com.wrapper.spotify.SpotifyHttpManager;
-import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 
 @Controller
 @ResponseBody
@@ -23,9 +20,23 @@ public class MyController{// implements ApplicationRunner{
 	@RequestMapping(value = "/")
 	public String hello() {
 	   //return gameUpdater.getGame().getItems();
-		SpotifyApi spotapi = new SpotifyApi;
-		return "suck my pp";
+		
+		Authenticator authenticator  = new Authenticator();
+		
+		return authenticator.authorizationCodeUri_Async();
+		
+		//return "suck my pp";
 	
+	}
+	@RequestMapping(value = "/callback")//, consumes = "text/plain" )
+	public String callBack() {//@RequestBody String payload) {
+		System.out.println("Something happened");
+		return "something happened";
+		//return payload;
+		
+		//Authenticator authenticator = new Authenticator();
+		
+		//return "";
 	}
 	/**
 	@Override
