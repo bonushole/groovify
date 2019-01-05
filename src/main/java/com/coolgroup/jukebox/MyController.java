4b59d356b390747f1804095799a30a28a1f4fb37
Maximilian Evans <156max@gmail.com>
2019-01-05 09:38:15 -0500

    added some real spotify API queries yo

--- src/main/java/com/coolgroup/jukebox/MyController.java
+++ src/main/java/com/coolgroup/jukebox/MyController.java
@@ -45,15 +45,16 @@ public class MyController{// implements ApplicationRunner{
 		return user.getToken();
 	}
 	@RequestMapping(value = "/addPlaylist")
-	public String AddPlaylist() {
+	public String AddPlaylist(@RequestParam("token") String token) {
 		
 		for(User user : users) {
-			if() {
-				
+			if(token.equals(user.getToken())) {
+				user.addPlaylist();
+				return "";
 			}
 		}
 		
-		return "";
+		return "error";
 	}
 	/**
 	@Override
